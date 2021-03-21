6878025f8efe4376c538c77c9852296ad62eb4dd
EtherWen <13640396937@163.com>
2021-03-21 22:50:33 +0800

    2021/03/21 调整一下目录结构

---
+++ geektime-kimmking-lessons-studying/src/main/java/jvmStudying/Hello.java
@@ -0,0 +1,14 @@
+package jvmStudying;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        int a=2,b=3;
+        int c = (b-a)*(a+b);
+
+        int[] arr = new int[b];
+        for (int i=0;i<b;i++){
+            arr[i] = c;
+        }
+    }
+}
