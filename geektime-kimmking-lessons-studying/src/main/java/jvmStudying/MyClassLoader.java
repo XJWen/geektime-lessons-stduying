706b790e463d630fe6fba2ce6747d84e3432bca3
EtherWen <13640396937@163.com>
2021-03-21 22:57:30 +0800

    2021/03/21 修改注释

--- geektime-kimmking-lessons-studying/src/main/java/jvmStudying/MyClassLoader.java
+++ geektime-kimmking-lessons-studying/src/main/java/jvmStudying/MyClassLoader.java
@@ -9,38 +9,31 @@ import java.lang.reflect.Method;
 public class MyClassLoader extends ClassLoader{
 
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-        // 相关参数
+
         final String className = "Hello";
         final String methodName = "hello";
-        // 创建类加载器
+        // 创建本地自定义类加载器
         ClassLoader classLoader = new MyClassLoader();
         Class<?> clazz = classLoader.loadClass(className);
-        // 创建对象
         Object instance = clazz.newInstance();
-        // 调用方法
         Method method = clazz.getMethod(methodName);
         method.invoke(instance);
     }
 
     @Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
+    protected Class<?> findClass(String pahtName) throws ClassNotFoundException {
         // 如果支持包名, 则需要进行路径转换
-        String resourcePath = name.replace(".", "/");
-        // 文件后缀
+        String resourcePath = pahtName.replace(".", "/");
         final String suffix = ".xlass";
-        // 获取输入流
         InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(resourcePath + suffix);
         try {
-            // 读取数据
             int length = inputStream.available();
             byte[] byteArray = new byte[length];
             inputStream.read(byteArray);
-            // 转换
             byte[] classBytes = decode(byteArray);
-            // 通知底层定义这个类
-            return defineClass(name, classBytes, 0, classBytes.length);
+            return defineClass(pahtName, classBytes, 0, classBytes.length);
         } catch (IOException e) {
-            throw new ClassNotFoundException(name, e);
+            throw new ClassNotFoundException(pahtName, e);
         } finally {
             close(inputStream);
         }
